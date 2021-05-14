e589b1838c0140ece13c088d9ea3226e84c06466
tanajim <69978761+tanajim@users.noreply.github.com>
2021-05-14 21:04:21 +0530

    Add files via upload
    4th

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: hypercorn "camus:create_app()" --log-file - -b 0.0.0.0:$PORT
---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='camus-chat',
+    version='0.3.1',
+    description='Peer-to-peer video chat using WebRTC',
+    long_description=open('README.rst').read(),
+    long_description_content_type="text/x-rst; charset=UTF-8",
+    url='https://camus.chat/',
+    download_url='https://pypi.org/project/camus-chat/',
+    project_urls={
+        'Code': 'https://github.com/mrgnr/camus',
+        'Issue tracker': 'https://github.com/mrgnr/camus/issues',
+        'Documentation': 'https://docs.camus.chat/',
+    },
+    author='Morgan Robertson',
+    author_email='mrgnr@pm.me',
+    license='AGPL-3.0-or-later',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'alembic==1.5.2',
+        'bootstrap-flask==1.3.1',
+        'flask-sqlalchemy==2.4.4',
+        'flask-wtf==0.14.3',
+        'hypercorn==0.11.1',
+        'python-slugify==4.0.1',
+        'quart==0.14.1',
+        'SQLAlchemy==1.3.23',
+        'twilio==6.45.4',
+        'werkzeug==1.0.1',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Environment :: Web Environment',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Communications :: Conferencing',
+        'Topic :: Multimedia :: Video',
+    ],
+    python_requires='>=3.7',
+    scripts=['bin/camus'],
+)
